666fa849e752d09af71fd25252224baf42d52328
Justin Gerber <Justbeingjustin@aol.com>
2018-12-18 20:55:17 -0800

    initial project push

---
+++ JSG.Email/Email.cs
@@ -0,0 +1,37 @@
+﻿using JSG.Email.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace JSG.Email
+{
+    public class Email
+    {
+        private readonly GoogleEmailDetails _googleEmailDetails;
+        private const string googleHost = "smtp.gmail.com";
+
+        public Email(GoogleEmailDetails googleEmailDetails)
+        {
+            _googleEmailDetails = googleEmailDetails;
+        }
+
+        /// <summary>
+        /// Sends an email to a recipient
+        /// </summary>
+        public void SendMail()
+        {
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = googleHost;
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(_googleEmailDetails.GoogleUserName, _googleEmailDetails.GooglePassword);
+            MailMessage mm = new MailMessage(_googleEmailDetails.From, _googleEmailDetails.To, _googleEmailDetails.Subject, _googleEmailDetails.Body);
+            mm.BodyEncoding = Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            client.Send(mm);
+        }
+    }
+}
---
+++ JSG.Email/Models/GoogleEmailDetails.cs
@@ -0,0 +1,35 @@
+﻿namespace JSG.Email.Models
+{
+    public class GoogleEmailDetails
+    {
+        /// <summary>
+        /// The username of the google account. Example: test@gmail.com
+        /// </summary>
+        public string GoogleUserName { get; set; }
+
+        /// <summary>
+        /// The password of the google account
+        /// </summary>
+        public string GooglePassword { get; set; }
+
+        /// <summary>
+        /// The email that will be used to send the message
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// The receipient of the email
+        /// </summary>
+        public string To { get; set; }
+
+        /// <summary>
+        /// The subject of the email
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// The body of the email
+        /// </summary>
+        public string Body { get; set; }
+    }
+}
